bef798c453608945e5539263cf121dc863e38739
Juan Marín <juanpablo.marin76@gmail.com>
2018-02-09 02:35:51 -0500

    docs(docusaurus): add docusaurus support

---
+++ website/siteConfig.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/* List of projects/orgs using your project for the users page */
+const users = [
+  {
+    caption: 'User1',
+    image: '/test-site/img/docusaurus.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'EvaluApp' /* title for your website */,
+  tagline: 'Generación de exámenes fácil',
+  url: 'https://facebook.github.io' /* your website url */,
+  baseUrl: '/evaluapp/' /* base url for your project */,
+  projectName: 'evaluapp',
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {blog: true, label: 'Blog'},
+  ],
+  users,
+  /* path to images for header/footer */
+  headerIcon: 'img/docusaurus.svg',
+  footerIcon: 'img/docusaurus.svg',
+  favicon: 'img/favicon.png',
+  /* colors for website */
+  colors: {
+    primaryColor: '#BA0F17',
+    secondaryColor: '#32BEA6',
+  },
+  // This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+  copyright:
+    'Copyright © ' +
+    new Date().getFullYear() +
+    ' Your Name or Your Company Name',
+  // organizationName: 'deltice', // or set an env variable ORGANIZATION_NAME
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks
+    theme: 'default',
+  },
+  scripts: ['https://buttons.github.io/buttons.js'],
+  // You may provide arbitrary config keys to be used as needed by your template.
+  repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
